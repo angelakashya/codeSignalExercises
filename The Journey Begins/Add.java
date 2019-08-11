ddf12d33f9b7390e29f76f470796fafadb3f72b3
Ângela Almeida <angella.akashya@gmail.com>
2019-08-11 00:10:00 -0300

    Ajuste de identação do código Add

--- The Journey Begins/Add.java
+++ The Journey Begins/Add.java
@@ -3,14 +3,15 @@
 */
 
 public class Add {
-    public static int add(int param1, int param2){
+
+    public static int add(int param1, int param2) {
         return param1 + param2; 
     }
+
     public static void main(String[] args) {
         System.out.println(add(1, 2));
         System.out.println(add(0, 1000));
         System.out.println(add(2, -37));
         System.out.println(add(-9, 10));
-    }
-    
+    }   
 }
